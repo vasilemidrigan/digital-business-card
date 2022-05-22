e18a64be17c4cc60b6cf93e413c25d7d9bdfb9fa
Vasile Midrigan <111rigan111@gmail.com>
2022-05-22 06:38:11 +0100

    build intro section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 
 import Contacts from "./components/Contacts";
+import Intro from "./components/Intro";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <div className="app__wrapper">
       <Contacts />
+      <Intro />
     </div>
   );
 }
---
+++ src/components/Intro.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Intro() {
+  return (
+    <div className="intro">
+      <div className="intro__about">
+        <h2>About</h2>
+        <p>
+          I am a frontend developer with a particular interest in making things
+          simple and automating daily tasks. I try to keep up with security and
+          best practices, and am always looking for new things to learn.
+        </p>
+      </div>
+      <div className="intro__interests">
+        <h2>Interests</h2>
+        <p>
+          Food expert. Music scholar. Reader. Internet fanatic. Bacon buff.
+          Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+        </p>
+      </div>
+    </div>
+  );
+}
